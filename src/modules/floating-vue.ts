18a44fbafff67f71513dd3033a94f28a247db657
adibarra <93070681+adibarra@users.noreply.github.com>
2022-10-15 18:12:26 -0500

    migrate to vue3-popper

--- src/modules/floating-vue.ts
+++
@@ -1,8 +0,0 @@
-import FloatingVue from 'floating-vue'
-import { type UserModule } from '~/types'
-
-import 'floating-vue/dist/style.css'
-
-export const install: UserModule = ({ app }) => {
-  app.use(FloatingVue)
-}
