1664a767fc642befddf50ef9a04ae999de818d5d
adibarra <93070681+adibarra@users.noreply.github.com>
2022-11-09 19:12:13 -0600

    clean up main page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -14,6 +14,11 @@ class University {
   }
 }
 
+const showPopper = ref(0)
+const setShowPopper = (value: number) => {
+  showPopper.value = value
+}
+
 const searchString = ref('')
 const currentUniversity = ref<University>(new University('', ''))
 const allUniversities = $ref<Array<University>>(new Array<University>())
@@ -61,12 +66,15 @@ fetchUniversities()
 </script>
 
 <template>
-  <div class="flex flex-col grow justify-evenly px-2">
-    <div class="flex flex-col p-2 mb-3">
-      <div class="mx-auto">
-        <br>
+  <main class="flex flex-col grow px-2">
+    <div class="h-[20vh]" />
+    <div class="flex flex-col p-2">
+      <div class="text-2xl font-900 my-5 sm:text-4xl">
+        The GDProject
+      </div>
+      <div class="mx-auto min-w-90 sm:min-w-120">
         <VSelect
-          class="style-chooser w-fit min-w-100 mx-auto"
+          class="style-chooser mx-auto"
           :options="allUniversities"
           :placeholder="t('main.select_university')"
           :filter-by="uniFilter"
@@ -74,7 +82,7 @@ fetchUniversities()
         />
         <div class="flex justify-center mt-2">
           <VSelect
-            class="style-chooser min-w-90"
+            class="style-chooser grow"
             placeholder="ex. CSCE 121 Fall 2022"
             :clear-search-on-blur="() => false"
             :no-drop="true"
@@ -88,47 +96,45 @@ fetchUniversities()
           </button>
         </div>
         <div class="flex justify-center mt-2 gap-5">
-          <Popper hover>
+          <Popper hover content=" " @open:popper="setShowPopper(1)">
             <span class="text-sm underline cursor-help">{{ t('main.not_sure') }}</span>
-            <template #content>
-              This website lets you check out the grade distribution of courses at your university.
-              <br>
-              There are a ton of good reasons to do this before picking a class or professor.
-              <br>
-              For more information check out the
-              <RouterLink class="icon-btn" to="/about" :title="t('link.about')">
-                about <div class="inline-block vertical-middle" i-carbon-help />
-              </RouterLink>
-              page.
-            </template>
           </Popper>
-          <Popper hover>
+          <Popper hover content=" " @open:popper="setShowPopper(2)">
             <span class="text-sm underline cursor-help">{{ t("main.missing_uni") }}</span>
-            <template #content>
-              Can't find your university in the university selection dropdown?
-              <br>
-              There is a pretty good chance that it hasn't been added yet.
-              <br>
-              Feel free to request it
-              <a class="icon-btn" href="https://github.com/GDProject/gd-parser/README#request-a-university/" title="{{ t('link.request_uni') }}">
-                here. <div class="inline-block vertical-middle" i-carbon-request-quote />
-              </a>
-            </template>
           </Popper>
-          <Popper hover>
-            <span class="text-sm underline cursor-help">How do I search?</span>
-            <template #content>
-              You must add a department name and course number.
-              <br>
-              You can attach a section number, semester, year, and professor.
-              <br>
-              EX: "CSCE 121 Fall 2022" or "CSCE 121.509 2022 Doe".
-            </template>
+          <Popper hover content=" " @open:popper="setShowPopper(3)">
+            <span class="text-sm underline cursor-help">{{ t("main.how_to_search") }}</span>
           </Popper>
         </div>
       </div>
+      <div class="flex flex-col text-justify op-75 mx-auto mt-5 max-w-90 sm:max-w-120">
+        <div v-show="showPopper === 1">
+          This website lets you check out the grade distribution of courses at your university.
+          There are a ton of good reasons to do this before picking a class or professor.
+          For more information check out the
+          <RouterLink class="icon-btn" to="/about" :title="t('link.about')">
+            about <div class="inline-block vertical-middle" i-carbon-help />
+          </RouterLink>
+          page.
+        </div>
+        <div v-show="showPopper === 2">
+          Can't find your university in the selection dropdown?
+          There is a pretty good chance that it hasn't been added yet.
+          Feel free to request it
+          <a class="icon-btn" href="https://github.com/GDProject/gd-parser/README#request-a-university/" title="{{ t('link.request_uni') }}">
+            here. <div class="inline-block vertical-middle" i-carbon-request-quote />
+          </a>
+        </div>
+        <div v-show="showPopper === 3">
+          First select your university in the first dropdown.
+          Then type in a department and course number in the second dropdown.
+          You can optionally include a section number, semester, year, and professor.
+          <br><br>
+          EX: "CSCE 121 Fall 2022" or "CSCE 121.509 2022 Smith".
+        </div>
+      </div>
     </div>
-  </div>
+  </main>
 </template>
 
 <style lang="scss">
