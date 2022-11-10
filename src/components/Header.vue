a2e7a9f70576c44c00e3a7462e48c87c64820b49
adibarra <93070681+adibarra@users.noreply.github.com>
2022-11-10 02:12:55 -0600

    continue cleaning up

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,29 +4,31 @@ const { t } = useI18n()
 
 <template>
   <header>
-    <div class="flex p-2 mb-3">
+    <div class="flex m-3">
       <RouterLink to="/" :title="t('link.home')">
         <div alt="{{ t('header.logo') }}" class="text-2xl sm:text-4xl" i-carbon-subflow />
       </RouterLink>
       <div class="grow" />
-      <button class="icon-btn m-auto ml-3 sm:ml-5 sm:text-xl !hidden" :title="t('header.select_lang')">
-        <div i-carbon-language />
-      </button>
-      <RouterLink class="icon-btn m-auto ml-3 sm:ml-5 sm:text-xl" to="/" :title="t('link.home')">
-        <div i-carbon-search />
-      </RouterLink>
-      <RouterLink class="icon-btn m-auto ml-3 sm:ml-5 sm:text-xl" to="/about" :title="t('link.about')">
-        <div i-carbon-help />
-      </RouterLink>
-      <a class="icon-btn m-auto ml-3 sm:ml-5 sm:text-xl" href="https://github.com/GDProject/gd-website" target="_blank" :title="t('link.github')">
-        <div i-carbon-logo-github />
-      </a>
-      <a class="icon-btn m-auto ml-3 sm:ml-5 sm:text-xl" href="mailto:gdproject@adibarra.com" target="_blank" :title="t('link.email')">
-        <div i-carbon-email />
-      </a>
-      <button class="icon-btn m-auto ml-3 sm:ml-5 sm:text-xl" :title="t('header.toggle_dark')" @click="toggleDark()">
-        <div i="carbon-sun dark:carbon-moon" />
-      </button>
+      <nav class="flex my-auto gap-x5 sm:text-xl">
+        <button class="icon-btn !hidden" :title="t('header.select_lang')">
+          <div i-carbon:language />
+        </button>
+        <RouterLink class="icon-btn" to="/" :title="t('link.home')">
+          <div i-carbon:search />
+        </RouterLink>
+        <RouterLink class="icon-btn" to="/about" :title="t('link.about')">
+          <div i-carbon:help />
+        </RouterLink>
+        <a class="icon-btn" href="https://github.com/GDProject/gd-website" target="_blank" :title="t('link.github')">
+          <div i-carbon:logo-github />
+        </a>
+        <a class="icon-btn" href="mailto:gdproject@adibarra.com" target="_blank" :title="t('link.email')">
+          <div i-carbon:email />
+        </a>
+        <button class="icon-btn" :title="t('header.toggle_dark')" @click="toggleDark()">
+          <div i="carbon:sun dark:carbon:moon" />
+        </button>
+      </nav>
     </div>
   </header>
 </template>
