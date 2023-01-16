e47f8398e6f61b20015df3a2a864c7845f8fc6f9
adibarra <93070681+adibarra@users.noreply.github.com>
2023-01-16 17:40:51 -0600

    style: match style to rest of project

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -34,9 +34,9 @@ const isHelpShown = (id: number) => helpText.value === id
                 <input
                   ref="courseSearch"
                   class="vs__search"
+                  autocomplete="off"
                   placeholder="ex. CSCE 121.203 Fall 2022 Smith, J."
                   :value="sState.course.valueRaw"
-                  autocomplete="off"
                   @input="sState.course.input"
                   @keydown.enter="sState.course.search"
                 >
@@ -44,11 +44,12 @@ const isHelpShown = (id: number) => helpText.value === id
               <div class="vs__actions">
                 <button
                   v-show="!sState.course.isEmpty"
-                  type="button"
+                  type="reset"
                   :title="t('button.clear')"
                   fill-black dark:fill-white cursor-pointer mr-1
                   @click="sState.course.clear"
                 >
+                  <!-- Close SVG -->
                   <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10">
                     <path d="M6.895455 5l2.842897-2.842898c.348864-.348863.348864-.914488 0-1.263636L9.106534.261648c-.348864-.348864-.914489-.348864-1.263636 0L5 3.104545 2.157102.261648c-.348863-.348864-.914488-.348864-1.263636 0L.261648.893466c-.348864.348864-.348864.914489 0 1.263636L3.104545 5 .261648 7.842898c-.348864.348863-.348864.914488 0 1.263636l.631818.631818c.348864.348864.914773.348864 1.263636 0L5 6.895455l2.842898 2.842897c.348863.348864.914772.348864 1.263636 0l.631818-.631818c.348864-.348864.348864-.914489 0-1.263636L6.895455 5z" />
                   </svg>
@@ -56,18 +57,20 @@ const isHelpShown = (id: number) => helpText.value === id
               </div>
             </div>
           </div>
-          <button class="hover:bg-[var(--light-2)] hover:dark:bg-[var(--dark-2)] border-[var(--dark-1)] dark:border-[var(--light-1)]" min-w-10 b-1 b-l-none rd-r-1>
-            <div :title="t('button.search')" flex h-full @click="sState.course.search">
-              <div i-carbon:search m-auto />
-            </div>
+          <button
+            class="hover:bg-[var(--light-2)] hover:dark:bg-[var(--dark-2)] border-[var(--dark-1)] dark:border-[var(--light-1)]"
+            min-w-10 b-1 b-l-none rd-r-1
+            @click="sState.course.search"
+          >
+            <div i-carbon:search :title="t('button.search')" flex h-full m-auto />
           </button>
         </div>
-        <div flex justify-center mt-2 gap-5>
-          <span text-sm underline cursor-help @mouseenter="showHelp(1)">{{ t('main.not_sure') }}</span>
-          <span text-sm underline cursor-help @mouseenter="showHelp(2)">{{ t("main.missing_uni") }}</span>
-          <span text-sm underline cursor-help @mouseenter="showHelp(3)">{{ t("main.how_to_search") }}</span>
+        <div flex mt-2 gap-5 justify-center text-sm underline>
+          <span cursor-help @mouseenter="showHelp(1)">{{ t('main.not_sure') }}</span>
+          <span cursor-help @mouseenter="showHelp(2)">{{ t("main.missing_uni") }}</span>
+          <span cursor-help @mouseenter="showHelp(3)">{{ t("main.how_to_search") }}</span>
         </div>
-        <div flex flex-col text-justify op-75 mx-auto mt-5 max-w-90 sm:max-w-120>
+        <div flex flex-col mx-auto mt-5 max-w-90 sm:max-w-120 text-justify op-75>
           <div v-show="isHelpShown(1)" @click.stop>
             This website lets you check out the grade distribution of courses at your university.
             There are a ton of good reasons to do this before picking a class or professor.
@@ -127,6 +130,7 @@ const isHelpShown = (id: number) => helpText.value === id
   }
   .vs__clear,
   .vs__open-indicator {
+    cursor: pointer;
     fill: var(--dark-1);
     html.dark & {
       fill: var(--light-1);
