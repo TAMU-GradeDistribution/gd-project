d2c9ea72f7c7526db8134e6a61a3e00fa36824f6
adibarra <93070681+adibarra@users.noreply.github.com>
2023-01-16 17:39:23 -0600

    style(404): match other pages

--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -1,17 +1,33 @@
 <script setup lang="ts">
 const { t } = useI18n()
 const router = useRouter()
+
+useHead({
+  title: `${t('error.not-found')} | GDProject`,
+  meta: [
+    { name: 'robots', content: 'noindex, nofollow' },
+  ],
+})
 </script>
 
 <template>
   <div grow flex flex-col>
-    <div class="bg-[var(--light-2)] dark:bg-[var(--dark-2)]" flex flex-col justify-center my-auto mx-auto p-5 rd-2 outline outline-1 outline-stone-700>
-      <div flex flex-row inline-block font-900 text-4xl>
+    <div h-20vh />
+    <div
+      class="bg-[var(--light-2)] dark:bg-[var(--dark-2)] outline-[var(--light-3)] dark:outline-[var(--dark-3)]"
+      flex flex-col mx-auto p-5 rd-2 outline outline-1
+    >
+      <div flex font-900 text-4xl>
         <span text-red>404:&nbsp;</span>
         {{ t('error.not-found') }}
       </div>
-      <button :title="t('link.go_back')" icon-btn flex flex-row text-lg w-fit mt-2 mr-auto @click="router.back()">
-        <div i-carbon:caret-left text-2xl />{{ t('link.go_back') }}
+      <button
+        :title="t('link.go_back')"
+        icon-btn flex text-lg mt-2 mr-auto
+        @click="router.back()"
+      >
+        <div i-carbon:caret-left text-2xl />
+        {{ t('link.go_back') }}
       </button>
     </div>
   </div>
