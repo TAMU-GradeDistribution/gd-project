864c2d8bd3ae96495bb385a635d2dc21412b2d41
adibarra <93070681+adibarra@users.noreply.github.com>
2023-01-17 01:54:52 -0600

    refactor: clean up header and footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,5 @@
 <template>
-  <footer>
-    <div text-center m-2 op-50 text-sm sm:text-base>
-      MIT Licensed | Made with ❤️ by The GDProject
-    </div>
+  <footer text-center m-2 op-50 text-sm sm:text-base>
+    MIT Licensed | Made with ❤️ by The GDProject
   </footer>
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,29 +3,49 @@ const { t } = useI18n()
 </script>
 
 <template>
-  <header>
-    <div flex m-3>
-      <RouterLink to="/" :title="t('link.home')">
-        <div :alt="t('header.logo')" i-carbon:subflow text-2xl sm:text-4xl />
+  <header flex m-3>
+    <RouterLink
+      to="/"
+      :title="t('header.logo')"
+    >
+      <div i-carbon:subflow text-2xl sm:text-4xl />
+    </RouterLink>
+    <div grow />
+    <nav flex my-auto gap-x5 sm:text-xl>
+      <RouterLink
+        to="/"
+        :title="t('link.home')"
+        icon-btn
+      >
+        <div i-carbon:home />
       </RouterLink>
-      <div grow />
-      <nav flex my-auto gap-x5 sm:text-xl>
-        <RouterLink to="/" :title="t('link.home')" icon-btn>
-          <div i-carbon:home />
-        </RouterLink>
-        <RouterLink to="/about" :title="t('link.about')" icon-btn>
-          <div i-carbon:help />
-        </RouterLink>
-        <button :title="t('header.select_lang')" icon-btn hidden>
-          <div i-carbon:language />
-        </button>
-        <a href="https://github.com/GDProject/gd-website" target="_blank" :title="t('link.github')" icon-btn>
-          <div i-carbon:logo-github />
-        </a>
-        <button :title="t('header.toggle_dark')" icon-btn @click="toggleDark()">
-          <div i="carbon-sun dark:carbon-moon" />
-        </button>
-      </nav>
-    </div>
+      <RouterLink
+        to="/about"
+        :title="t('link.about')"
+        icon-btn
+      >
+        <div i-carbon:help />
+      </RouterLink>
+      <button
+        :title="t('header.select_lang')"
+        icon-btn hidden
+      >
+        <div i-carbon:language />
+      </button>
+      <a
+        href="https://github.com/GDProject/gd-website"
+        target="_blank" :title="t('link.github')"
+        icon-btn
+      >
+        <div i-carbon:logo-github />
+      </a>
+      <button
+        :title="t('header.toggle_dark')"
+        icon-btn
+        @click="toggleDark()"
+      >
+        <div i="carbon-sun dark:carbon-moon" />
+      </button>
+    </nav>
   </header>
 </template>
