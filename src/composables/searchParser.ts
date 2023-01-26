8a54b1ee58cc3d2250b563659575412bca895950
adibarra <93070681+adibarra@users.noreply.github.com>
2023-01-26 16:08:04 -0600

    feat: improve search parser

--- src/composables/searchParser.ts
+++ src/composables/searchParser.ts
@@ -1,28 +1,111 @@
-export const searchParser = (university: string, queryString: string) => {
-  const query = { uni: university, dep: '', course: '', sec: '', year: '', sem: '', prof: '' }
+export const searchParser = (uni: string, query: string) => {
+  const search = {
+    university: uni,
+    dep: '',
+    course: '',
+    sec: '',
+    year: '',
+    sem: '',
+    prof: '',
+  }
 
-  queryString.split(' ').forEach((word: string) => {
+  // only keep letters, numbers, accents, and some punctuation
+  const sanitize = (str: string) => str.replace(/[^\p{L}\p{M}\d'., ]/giu, '')
+
+  // remove duplicate whitespace and trim
+  const trimSpace = (str: string) => str.replace(/\s{2,}/g, ' ').trim()
+
+  // convert to start case
+  const toStartCase = (str: string) => str.split(' ')
+    .filter(s => s)
+    .map(s => `${s[0].toUpperCase()}${s.substring(1).toLowerCase()}`)
+    .join(' ')
+
+  // match 'SPRING', 'SUMMER', or 'FALL'
+  const semRegex = /^(?:SPRING)|(?:SUMMER)|(?:FALL)$/gi
+
+  // match 2-4 capital letters
+  const depRegex = /^[A-Z]{2,4}$/gi
+
+  // match 3-4 digits, optionally followed by a dot and 3-4 digits, optionally followed by a capital letter
+  const courseRegex = /^\d{3,4}(?:\.\d{3,4}[A-Z]?)?$/gi
+
+  // match 4 digits
+  const yearRegex = /^\d{4}$/g
+
+  // clean up and split query into tokens
+  const tokens = trimSpace(sanitize(query)).split(' ')
+
+  // determine the type for each token
+  tokens.forEach((token: string) => {
+    const hasDot = !!token.indexOf('.')
+
     switch (true) {
-      case query.dep.length === 0 && word.length === 4 && isNaN(Number(word)) && !['FALL', 'SPRING', 'SUMMER'].includes(word.toLocaleUpperCase()):
-        query.dep = word
+      // test for semester
+      case !search.sem.length && semRegex.test(token):
+        search.sem = token
         break
-      case query.course.length === 0 && word.length >= 3 && word.length !== 4 && (!isNaN(Number(word)) || word.includes('.')):
-        query.course = word.includes('.') ? word.split('.')[0] : word
-        query.sec = word.includes('.') ? word.split('.')[1] : ''
+
+      // test for department
+      case !search.dep.length && depRegex.test(token):
+        search.dep = token
         break
-      case query.year.length === 0 && word.length === 4 && !isNaN(Number(word)):
-        query.year = word
+
+      // test for course
+      case !search.course.length && courseRegex.test(token):
+        search.course = hasDot ? token.split('.')[0] : token
+        search.sec = hasDot ? token.split('.')[1] : ''
         break
-      case query.sem.length === 0 && ['FALL', 'SPRING', 'SUMMER'].includes(word.toLocaleUpperCase()):
-        query.sem = word
+
+      // test for year
+      case !search.year.length && yearRegex.test(token):
+        search.year = token
         break
+
+      // else, it's probably a prof name
       default:
-        query.prof += `${word} `
+        search.prof += `${token} `
     }
   })
 
-  query.prof = query.prof.trim()
+  // run some additional checks
 
-  // remove empty keys to reduce url query length
-  return Object.fromEntries(Object.entries(query).filter(([_, v]) => v !== ''))
+  // if year and dep but no course, then year is probably a course
+  if (search.year.length && search.dep.length && !search.course.length) {
+    search.course = search.year
+    search.year = ''
+  }
+
+  // if dep but no course, then dep is probably a prof name
+  if (search.dep.length && !search.course.length) {
+    search.prof = `${search.dep} ${search.prof}`
+    search.dep = ''
+  }
+
+  // if course but no dep, then course is probably a year
+  if (search.course.length && !search.dep.length) {
+    search.year = search.course
+    search.course = ''
+  }
+
+  // make sure search object is formatted correctly
+  search.sem = toStartCase(search.sem)
+  search.dep = search.dep.toUpperCase()
+  search.course = search.course.toUpperCase()
+  search.sec = search.sec.toUpperCase()
+  search.prof = toStartCase(search.prof)
+
+  // remove empty keys and truncate key names to 1 character
+  const result = Object.fromEntries(
+    Object.entries(search)
+      .filter(([_, v]) => v)
+      .map(([k, v]) => [k[0], v]),
+  )
+
+  // print the search object for debugging
+  Object.entries(result)
+    .forEach(([k, v]) => console.log(`${k}: ${v}`))
+  return result
 }
+
+'asdf asdf asdf'[0]
