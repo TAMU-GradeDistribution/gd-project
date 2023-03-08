09067a4a27ef44543d46f45ee505afe81e31d5e8
adibarra <93070681+adibarra@users.noreply.github.com>
2023-03-08 16:53:43 -0600

    feat: add more dotenv vars

--- packages/client/src/composables/config.ts
+++ packages/client/src/composables/config.ts
@@ -1,14 +1,15 @@
-import { dirname, resolve } from 'path'
-import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { config } from 'dotenv'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 config({
-  path: resolve(__dirname, '../../../.client.env'),
+  path: resolve(__dirname, '../../../.public.env'),
 })
 
 export const CONFIG = {
   API_HOST: process.env.API_HOST!,
   API_PORT: process.env.API_PORT!,
+  API_PATH: process.env.API_PATH!,
   CLIENT_HOST: process.env.CLIENT_HOST!,
 }
