688fd8dcbf969964dca226049896d2bce3711a88
adibarra <93070681+adibarra@users.noreply.github.com>
2023-03-08 18:13:00 -0600

    feat(server): add more dummy data

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -21,6 +21,23 @@ app.get(`${CONFIG.API_PATH}/universities`, (req, res) => {
   })
 })
 
+app.get(`${CONFIG.API_PATH}/:universityId/professors`, (req, res) => {
+  // some dummy data, replace with a database query
+  if (req.params.universityId === 'TAMU') {
+    res.json({
+      data: [
+        { id: '0', universityId: 'TAMU', firstName: 'John', lastName: 'Doe' },
+        { id: '1', universityId: 'TAMU', firstName: 'Jane', lastName: 'Doe' },
+      ],
+    })
+  }
+  else {
+    res.json({
+      data: [],
+    })
+  }
+})
+
 // listen for requests
 app.listen(CONFIG.API_PORT, () => {
   console.log(`API server is now listening on ${CONFIG.API_HOST}:${CONFIG.API_PORT}`)
